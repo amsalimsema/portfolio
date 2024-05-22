e42961098024feedbcfa329bb6b2a71ff3e1efad
amsalimsema <salimsema@yahoo.com>
2024-05-22 10:37:52 +0300

    secondCommit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   }
 
   const handleCopy = () => {
-    const text = 'hsu@jsmastery.pro'
+    const text = 'realsalimsema@gmail.com'
     navigator.clipboard.writeText(text)
     setCopied(true)
   }
